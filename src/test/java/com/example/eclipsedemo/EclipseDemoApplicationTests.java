2e917e2c586afda7abc77ce4cecde00d9295c146
kumasaka <kumasaka9732@gmal.com>
2020-11-21 12:07:44 +0900

    first commit

---
+++ src/test/java/com/example/eclipsedemo/EclipseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eclipsedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclipseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
